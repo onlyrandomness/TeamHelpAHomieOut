c581d9be07f1252a62babbe5089ce9bbd9770d2b
Mark Mocek <markmocek@yahoo.com>
2018-04-16 16:30:44 -0700

    New base project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBoFObH4FDPs7XhPzegnUEMqYz1dN1Bbl0',
+    authDomain: 'helpahomieout-9414f.firebaseapp.com',
+    databaseURL: 'https://helpahomieout-9414f.firebaseio.com',
+    projectId: 'helpahomieout-9414f',
+    storageBucket: '',
+    messagingSenderId: '466897444830'
+  }
+};
