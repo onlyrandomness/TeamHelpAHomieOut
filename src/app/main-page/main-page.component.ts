32ec31bc71a997a4d8b0ec807630ef60a540b6c1
Mark Mocek <markmocek@yahoo.com>
2018-05-04 16:54:06 -0700

    Main-Page SubmitButton
    added submit function for the main-page submit button. currently placeholder until api call is finished.

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
+  apiSpace: string = "<b>Some html.</b>";
+  
   ngOnInit() {
   }
 
+  
+  
+  onSubmit(e) {
+    event.preventDefault();
+    this.apiSpace = "<table style='width:100%'><tr><th>Example Data</th><th>Example Text</th></tr><tr><td>Data</td><td>Text</td></tr></table>";
+  }
+  
+  
 }
