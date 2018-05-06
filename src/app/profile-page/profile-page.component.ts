5df33d6960b0cddd0705122c5cfc93624fe31cf9
Mark Mocek <markmocek@yahoo.com>
2018-05-06 02:43:57 -0700

    Profile-Page
    profile page is set up with firebase, currently hardcoded in the user until login page is complete.

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -7,12 +7,25 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
-  profileObservable: Observable<any[]>;
+  user = {
+    name: null,
+    email: null,
+    favFoods: null,
+    idUrl: null,
+    password: null
+  }
   constructor(private db: AngularFireDatabase) { }
   ngOnInit() {
-    this.profileObservable = this.getProfile('/users/' + 'Admin');
+    this.getProfile('/users/' + 'Admin').subscribe( x => {
+      console.log(x)
+      this.user.name = x.name
+      this.user.email = x.email
+      this.user.favFoods = x.favfoods
+      this.user.idUrl = x.idUrl
+      this.user.password = x.password
+    })
   }
-  getProfile(listPath): Observable<any[]> {
-    return this.db.list(listPath).valueChanges();
+  getProfile(listPath): Observable<any> {
+    return this.db.object(listPath).valueChanges();
   }
 }
