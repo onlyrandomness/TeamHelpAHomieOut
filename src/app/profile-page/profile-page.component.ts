00b410a020cac15aeb23c765fc17d33c7e19f1be
Mark Mocek <markmocek@yahoo.com>
2018-05-07 16:58:59 -0700

    Profile-Page Base
    profile page is now able to pull current user from a service. Aside from changing user information, the page is good to go. May change userID to string instead of an array.

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database'; 
 import { Observable } from 'rxjs/Observable';
+import {DataService} from '../services/data.service';
+
 @Component({
   selector: 'profile-page',
   templateUrl: 'profile-page.component.html',
@@ -14,9 +16,16 @@ export class ProfilePageComponent implements OnInit {
     idUrl: null,
     password: null
   }
-  constructor(private db: AngularFireDatabase) { }
+  
+  userID = [];
+  
+  constructor(private db: AngularFireDatabase, private _data: DataService) { }
+  
   ngOnInit() {
-    this.getProfile('/users/' + 'Admin').subscribe( x => {
+    this._data.currentUser.subscribe(res => this.userID = res);
+    this._data.changeCurrentUser(this.userID);
+    
+    this.getProfile('/users/' + this.userID[0]).subscribe( x => {
       console.log(x)
       this.user.name = x.name
       this.user.email = x.email
