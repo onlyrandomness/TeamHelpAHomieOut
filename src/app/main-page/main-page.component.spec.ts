f18971a484d4a43b152c2a2f10d9e673c71128f3
johnjing <838633350@qq.com>
2018-05-08 14:36:27 -0700

    edited main page

--- src/app/main-page/main-page.component.spec.ts
+++ src/app/main-page/main-page.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainPageComponent } from './main-page.component';
 
+
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
